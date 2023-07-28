0c3e67f87e395634d106526212293af6f5ada960
Zeinab Abdien <suzanabdien15@gmail.com>
2023-07-28 22:55:14 +0300

    add delete query

--- app/src/main/java/com/iti/android_4/data/lacal/db/dao/SavedQuoteDao.kt
+++ app/src/main/java/com/iti/android_4/data/lacal/db/dao/SavedQuoteDao.kt
@@ -1,9 +1,6 @@
 package com.iti.android_4.data.lacal.db.dao
 
-import androidx.room.Dao
-import androidx.room.Insert
-import androidx.room.OnConflictStrategy
-import androidx.room.Query
+import androidx.room.*
 import com.iti.android_4.models.saved.SavedQuoteLocalDataModel
 
 
@@ -15,5 +12,8 @@ interface SavedQuoteDao {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun insertNewQuote(saveNewQuote: SavedQuoteLocalDataModel)
+
+    @Query("DELETE FROM Chats WHERE quote = :quote AND author = :author")
+    suspend fun deleteQuote(quote: String, author: String)
 
 }
