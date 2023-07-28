c37ebef42c73d4e8354cda5315e203c988641d42
Zeinab Abdien <suzanabdien15@gmail.com>
2023-07-28 23:18:25 +0300

    add base repository and handle save code

--- app/src/main/java/com/iti/android_4/ui/saved/SavedQuoteViewModel.kt
+++ app/src/main/java/com/iti/android_4/ui/saved/SavedQuoteViewModel.kt
@@ -1,36 +1,34 @@
 package com.iti.android_4.ui.saved
 
-import android.util.Log
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.iti.android_4.models.saved.SavedQuoteLocalDataModel
+import com.iti.android_4.ui.BaseRepository
 import kotlinx.coroutines.launch
 
 class SavedQuoteViewModel(
-    private val quotesRepository: SavedQuotesRepository
+    private val savedRepository: BaseRepository
 ) : ViewModel() {
-
 
     private val _savedQuotes: MutableLiveData<List<SavedQuoteLocalDataModel>> = MutableLiveData()
     val savedQuotes: MutableLiveData<List<SavedQuoteLocalDataModel>> = _savedQuotes
 
-
-    fun getQuotess() {
+    fun getRetrofitQuotes() {
         viewModelScope.launch {
             savedQuotes()
         }
     }
 
-
     private suspend fun savedQuotes() {
 
-        _savedQuotes.postValue(quotesRepository.getSavedQuotes())
-        _savedQuotes.value = quotesRepository.getSavedQuotes()
-        Log.d("suz", savedQuotes.value.toString() + " savedQuotes")
-        Log.d("suz", quotesRepository.getSavedQuotes().toString() + " getSavedQuotes")
+        _savedQuotes.postValue(savedRepository.getSavedQuotes())
+        _savedQuotes.value = savedRepository.getSavedQuotes()
 
     }
 
+    suspend fun deleteQuote(quote: String , author :String){
+        savedRepository.deleteQuotes(quote = quote, author = author)
+    }
 
 }
